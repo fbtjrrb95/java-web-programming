8c54705dedddad828e9b0197c634e2feb9afccb8
mac <tjrrb95@gmail.com>
2021-07-09 23:28:58 +0900

    test code 작성 (add, subtract, multiply, divide)

--- src/test/java/me/screw/javawebprogramming/calculator/CalculatorTest.java
+++ src/test/java/me/screw/javawebprogramming/calculator/CalculatorTest.java
@@ -8,17 +8,25 @@ class CalculatorTest {
 
     @Test
     void add() {
+        Calculator cal = new Calculator();
+        assertEquals(9,cal.add(6,3));
     }
 
     @Test
     void subtract() {
+        Calculator cal = new Calculator();
+        assertEquals(3, cal.subtract(6,3));
     }
 
     @Test
     void multiply() {
+        Calculator cal = new Calculator();
+        assertEquals(18,cal.multiply(6,3));
     }
 
     @Test
     void divide() {
+        Calculator cal = new Calculator();
+        assertEquals(2,cal.divide(6,3));
     }
 }
