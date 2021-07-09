e6e2d6c2558e52607e99fe4ca0a11ee49f506bfd
mac <tjrrb95@gmail.com>
2021-07-09 23:23:49 +0900

    Initial commit

---
+++ src/main/java/me/screw/javawebprogramming/JavaWebProgrammingApplication.java
@@ -0,0 +1,13 @@
+package me.screw.javawebprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/screw/javawebprogramming/JavaWebProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.screw.javawebprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
