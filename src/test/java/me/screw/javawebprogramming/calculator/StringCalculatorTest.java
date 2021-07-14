9cc75cf3682b9b03ca6c9e34714623205581d2ed
mac <tjrrb95@gmail.com>
2021-07-14 23:09:41 +0900

    StringCalculator test code ~#4 작성

--- src/test/java/me/screw/javawebprogramming/calculator/StringCalculatorTest.java
+++ src/test/java/me/screw/javawebprogramming/calculator/StringCalculatorTest.java
@@ -39,5 +39,11 @@ public class StringCalculatorTest {
         assertEquals(6,stringCalculator.add("1,2:3"));
     }
 
+    @Test
+    public void add_custom_delimeter(){
+        assertEquals(6,stringCalculator.add("//]\n1]2]3"));
+        assertEquals(6,stringCalculator.add("//-\n1-2-3"));
+    }
+
 
 }
